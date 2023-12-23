2815e134e049adf31824c663987d879918d842ba
akshay-nimbokar <152813686+akshay-nimbokar@users.noreply.github.com>
2023-12-24 01:21:31 +0530

    java pratice program

---
+++ src/test/java/JavaPractice/Akshay/ForLoop2.java
@@ -0,0 +1,15 @@
+package JavaPractice.Akshay;
+
+public class ForLoop2{
+
+	public static void main(String[] args) {
+		//Print from 2 to 20
+		
+		for(int i=2; i<=20; i= i+2)
+		{
+			System.out.println(i);
+		}
+
+	}
+
+}
