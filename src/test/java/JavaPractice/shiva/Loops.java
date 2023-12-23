990903b3c1486ebf4f56f9d7c760fe13f87d7ba6
Shivanand Jangam <shivanand@infoglen.com>
2023-12-23 22:40:59 +0530

    Changes for statements

--- src/test/java/JavaPractice/shiva/Loops.java
+++ src/test/java/JavaPractice/shiva/Loops.java
@@ -36,10 +36,34 @@ public class Loops {
 		}
 	}
 
+	public void whileLoopExample() {
+
+		int count = 10;
+		// List<String> myList= new ArrayList<String>();
+		// for(String myValue:myList) {
+		//
+		// }
+
+		while (count < 10) {
+			System.out.println(" While: "+count);
+			count++;
+		}
+	}
+
+	public void doWhileExample() {
+		int i = 10;
+		do {
+			System.out.println("Do While: "+i);
+			i++;
+		} while (i <= 10);
+	}
+
 	public static void main(String[] args) {
 		Loops loops = new Loops();
-		loops.forLoopExample();
-//		loops.forEachLoopExample();
+		// loops.forLoopExample();
+		// loops.forEachLoopExample();
+		 loops.whileLoopExample();
+		loops.doWhileExample();
 
 	}
 
