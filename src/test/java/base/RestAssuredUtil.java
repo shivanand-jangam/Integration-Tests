0a336287067d9f9de2e2b821fdd6a711db392b8b
Shivanand Jangam <shivanand@infoglen.com>
2023-12-29 10:09:19 +0530

    Automated API testcase

--- src/test/java/base/RestAssuredUtil.java
+++ src/test/java/base/RestAssuredUtil.java
@@ -1,5 +1,8 @@
 package base;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.testng.Assert;
 
 import io.restassured.RestAssured;
@@ -17,9 +20,25 @@ public class RestAssuredUtil {
 
 	static Response response;
 	static String TOKEN;
+	static String END_POINT;
+	static Map<String, String> headers = new HashMap<>();
+	static String REQUEST_BODY;
 
 	public RestAssuredUtil() {
 		RestAssured.baseURI = fileUtil.getProperty("api.base.uri");
+	}
+
+	public void setHeaders(String key, String value) {
+		headers.put(key, value);
+		headers.put("Authorization", "token=" + TOKEN);
+	}
+
+	public void setBody(String body) {
+		REQUEST_BODY = body;
+	}
+
+	public void setEndpoint(String endpoint) {
+		END_POINT = endpoint;
 	}
 
 	public static void triggerAPICall(String requestMethod, String endpoint,
@@ -27,46 +46,70 @@ public class RestAssuredUtil {
 		generateToken();
 		if (requestMethod.equalsIgnoreCase("POST")) {
 			System.out.println("Requesting a CALL for endpoint: " + endpoint);
-			response = RestAssured.given().when()
-					.header("Content-Type", ContentType.JSON)
-					.header("Authorization", "token=" + TOKEN).body(body)
+			response = RestAssured.given().when().headers(headers).body(body)
 					.post(endpoint);
 			System.out.println(response.asPrettyString());
 
 		} else if (requestMethod.equalsIgnoreCase("PUT")) {
 			System.out.println("Requesting a CALL for endpoint: " + endpoint);
-			response = RestAssured.given().when()
-					.header("Content-Type", ContentType.JSON)
-					.header("Authorization", "token=" + TOKEN).body(body)
+			response = RestAssured.given().when().headers(headers).body(body)
 					.put(endpoint);
 			System.out.println(response.asPrettyString());
 
 		} else if (requestMethod.equalsIgnoreCase("PATCH")) {
 			System.out.println("Requesting a CALL for endpoint: " + endpoint);
-			response = RestAssured.given().when()
-					.header("Content-Type", ContentType.JSON)
-					.header("Authorization", "token=" + TOKEN).body(body)
+			response = RestAssured.given().when().headers(headers).body(body)
 					.patch(endpoint);
 			System.out.println(response.asPrettyString());
 
 		} else if (requestMethod.equalsIgnoreCase("GET")) {
 			System.out.println("Requesting a CALL for endpoint: " + endpoint);
-			response = RestAssured.given().when()
-					.header("Content-Type", ContentType.JSON)
-					.header("Authorization", "Bearer " + TOKEN).body(body)
+			response = RestAssured.given().when().headers(headers).body(body)
 					.get(endpoint);
 			System.out.println(response.asPrettyString());
 
 		} else if (requestMethod.equalsIgnoreCase("DELETE")) {
 			System.out.println("Requesting a CALL for endpoint: " + endpoint);
-			response = RestAssured.given().when()
-					.header("Content-Type", ContentType.JSON)
-					.header("Authorization", "token=" + TOKEN).body(body)
+			response = RestAssured.given().when().headers(headers).body(body)
 					.delete(endpoint);
 			System.out.println(response.asPrettyString());
 		}
 	}
-	
+
+	public void triggerAPICall(String requestMethod) {
+		generateToken();
+		if (requestMethod.equalsIgnoreCase("POST")) {
+			System.out.println("Requesting a CALL for endpoint: " + END_POINT);
+			response = RestAssured.given().when().headers(headers)
+					.body(REQUEST_BODY).post(END_POINT);
+			System.out.println(response.asPrettyString());
+
+		} else if (requestMethod.equalsIgnoreCase("PUT")) {
+			System.out.println("Requesting a CALL for endpoint: " + END_POINT);
+			response = RestAssured.given().when().headers(headers)
+					.body(REQUEST_BODY).put(END_POINT);
+			System.out.println(response.asPrettyString());
+
+		} else if (requestMethod.equalsIgnoreCase("PATCH")) {
+			System.out.println("Requesting a CALL for endpoint: " + END_POINT);
+			response = RestAssured.given().when().headers(headers)
+					.body(REQUEST_BODY).patch(END_POINT);
+			System.out.println(response.asPrettyString());
+
+		} else if (requestMethod.equalsIgnoreCase("GET")) {
+			System.out.println("Requesting a CALL for endpoint: " + END_POINT);
+			response = RestAssured.given().when().headers(headers)
+					.body(REQUEST_BODY).get(END_POINT);
+			System.out.println(response.asPrettyString());
+
+		} else if (requestMethod.equalsIgnoreCase("DELETE")) {
+			System.out.println("Requesting a CALL for endpoint: " + END_POINT);
+			response = RestAssured.given().when().headers(headers)
+					.body(REQUEST_BODY).delete(END_POINT);
+			System.out.println(response.asPrettyString());
+		}
+	}
+
 	public static void generateToken() {
 		String endpoint = "/auth";
 		String body = "{\"username\" : " + "\""
@@ -78,32 +121,32 @@ public class RestAssuredUtil {
 				.post(endpoint);
 		Assert.assertEquals(response.getStatusCode(), 200);
 		TOKEN = response.jsonPath().get("token");
-		System.out.println(TOKEN);
+		System.out.println("Generated Token is : " + TOKEN);
 	}
 
 	public void verifyResonseCodeAs(int statusCode) {
 		Assert.assertEquals(response.getStatusCode(), statusCode);
+		System.out.println("Verified status code as: " + statusCode);
 	}
-	
-	
+
 	// public static void main(String[] args) {
-		//
-		// RestAssuredUtil assuredUtil = new RestAssuredUtil();
-		//
-		// assuredUtil.generateToken();
-		//
-		// String body2 =
-		// "{\"firstname\":\"TestFname\",\"lastname\":\"TestLname\",\"totalprice\":252525,\"depositpaid\":true,\"bookingdates\":{\"checkin\":\"2018-01-01\",\"checkout\":\"2019-01-01\"},\"additionalneeds\":\"Breakfast\"}";
-		//
-		// triggerAPICall("POST", "/booking", body2);
-		//
-		// Assert.assertEquals(response.getStatusCode(), 200);
-		//
-		// int bookingId = response.jsonPath().get("bookingid");
-		// System.out.println(bookingId);
-		//
-		// triggerAPICall("GET", "/booking/" + bookingId, "");
-		//
-		// }
+	//
+	// RestAssuredUtil assuredUtil = new RestAssuredUtil();
+	//
+	// assuredUtil.generateToken();
+	//
+	// String body2 =
+	// "{\"firstname\":\"TestFname\",\"lastname\":\"TestLname\",\"totalprice\":252525,\"depositpaid\":true,\"bookingdates\":{\"checkin\":\"2018-01-01\",\"checkout\":\"2019-01-01\"},\"additionalneeds\":\"Breakfast\"}";
+	//
+	// triggerAPICall("POST", "/booking", body2);
+	//
+	// Assert.assertEquals(response.getStatusCode(), 200);
+	//
+	// int bookingId = response.jsonPath().get("bookingid");
+	// System.out.println(bookingId);
+	//
+	// triggerAPICall("GET", "/booking/" + bookingId, "");
+	//
+	// }
 
 }
---
+++ src/test/java/stepDefinations/api/APIStepDefination.java
@@ -0,0 +1,38 @@
+package stepDefinations.api;
+
+import base.RestAssuredUtil;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class APIStepDefination {
+
+	RestAssuredUtil restAssuredUtil = new RestAssuredUtil();
+
+	@Given("I have the API endpoint {string}")
+	public void i_have_the_api_endpoint(String endpoint) {
+		restAssuredUtil.setEndpoint(endpoint);
+	}
+
+	@When("I provided the body for create booking")
+	public void i_provided_the_body_for_create_booking() {
+		String body = "{\"firstname\":\"ApiFName\",\"lastname\":\"ApiLName\",\"totalprice\":656565,\"depositpaid\":true,\"bookingdates\":{\"checkin\":\"2024-01-01\",\"checkout\":\"2024-01-01\"},\"additionalneeds\":\"Breakfast\"}";
+		restAssuredUtil.setBody(body);
+	}
+
+	@When("I provided the content type as {string}")
+	public void i_provided_the_content_type_as(String string) {
+		restAssuredUtil.setHeaders("Content-Type", "application/json");
+	}
+
+	@When("I make a {string} request")
+	public void i_make_a_request(String requestType) {
+		restAssuredUtil.triggerAPICall(requestType);
+	}
+
+	@Then("the response status code should be {int}")
+	public void the_response_status_code_should_be(Integer statucCode) {
+		restAssuredUtil.verifyResonseCodeAs(statucCode);
+	}
+
+}
